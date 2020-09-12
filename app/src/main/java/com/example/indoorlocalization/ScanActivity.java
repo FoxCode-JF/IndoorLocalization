28b2cac4612285ba9cae14c242b76d4429f0b33f
Fox <filipiukjarek@gmail.com>
2020-09-13 00:47:25 +0200

    Basic distance calculation, calibrate button needs fixing

--- app/src/main/java/com/example/indoorlocalization/ScanActivity.java
+++ app/src/main/java/com/example/indoorlocalization/ScanActivity.java
@@ -3,50 +3,65 @@ package com.example.indoorlocalization;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.MacAddress;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.HashMap;
-import java.util.Vector;
-import java.util.logging.LogRecord;
 
 public class ScanActivity extends AppCompatActivity {
-
-
 
     final static String TAG = "INDOOR_LOCALIZATION: ";
     private int REQUEST_ENABLE_BT = 1;
     private BluetoothAdapter bluetoothAdapter;
+    private int txPower = -60;
 
     private boolean mScanning;
-//    private Handler handler = new Handler();
-//    private static final long SCAN_PERIOD = 10000; // Stops scanning after 10 seconds.
+    private boolean mCalibrated;
 
-    HashMap<String, Integer> MACandRSSIMap = new HashMap<String, Integer>() {
+    private ScanResult mScanResult;
+    private Handler handler = new Handler();
+    private static final long SCAN_PERIOD = 10000; // Stops scanning after 10 seconds.
+
+    static HashMap<String, Integer> MACandRSSIMap = new HashMap<String, Integer>() {
         {
-            put("E4:34:93:81:24:D9", 1);
-            put("CC:98:8B:CF:BC:82", 1);
-
+            //put("E4:34:93:81:24:D9", -60);
+            put("CC:98:8B:CF:BC:82", -60);
         }
     };
 
+    private Button calibrateBtn;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.scan_activity);
 
+//        calibrateBtn = (Button) findViewById(R.id.buttonCalibrate);
+      /*  calibrateBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.e(TAG, "KALIBRACJA!!!");
+            }
+        });*/
 
+ /*       calibrateBtn = (Button) findViewById(R.id.buttonCalibrate);
 
+        calibrateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "KALIBRACJA!!!");
+            }
+        });*/
         final BluetoothManager bluetoothManager =(BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
 
@@ -69,7 +84,7 @@ public class ScanActivity extends AppCompatActivity {
         scanLeDevice();
     }
     // Device scan callback.
-    private BluetoothAdapter.LeScanCallback leScanCallback = new BluetoothAdapter.LeScanCallback() {
+   /* private BluetoothAdapter.LeScanCallback leScanCallback = new BluetoothAdapter.LeScanCallback() {
         @Override
         public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
             if(MACandRSSIMap.containsKey(device.getAddress()))
@@ -78,9 +93,31 @@ public class ScanActivity extends AppCompatActivity {
             }
 
         }
-    };
+    };*/
 
-    private void scanLeDevice() {
+    private ScanCallback leScanCallback =
+            new ScanCallback() {
+                @Override
+                public void onScanResult(int callbackType, final ScanResult result) {
+                    super.onScanResult(callbackType, result);
+                    determineTxPower(result);
+
+                    mScanResult = result;
+
+                    if(!mCalibrated){
+                        if(MACandRSSIMap.containsKey(result.getDevice().getAddress())) {
+                            Log.e(TAG, "onLeScan: Address: " + result.getDevice().getAddress() +
+                                    " Name: " + result.getDevice().getName() +
+                                    " RSSI: " + result.getRssi() +
+                                    " txPower: " + txPower +
+                                    " Distance: " + calculateDistance(result.getRssi(), txPower)
+                            );
+                        }
+                    }
+                }
+            };
+
+/*    private void scanLeDevice() {
         if (!mScanning) {
             // Stops scanning after a pre-defined scan period.
             new Runnable() {
@@ -97,6 +134,64 @@ public class ScanActivity extends AppCompatActivity {
             mScanning = false;
             bluetoothAdapter.stopLeScan(leScanCallback);
         }
+    }*/
+
+    private void scanLeDevice() {
+        if (!mScanning) {
+            new Runnable() {
+                @Override
+                public void run() {
+                    stopLeScan(bluetoothAdapter.getBluetoothLeScanner());
+                }
+            };
+
+            startLeScan(bluetoothAdapter.getBluetoothLeScanner());
+        } else {
+            stopLeScan(bluetoothAdapter.getBluetoothLeScanner());
+        }
+    }
+
+    private void startLeScan(BluetoothLeScanner leScanner) {
+        mScanning = true;
+        leScanner.startScan(leScanCallback);
+    }
+
+    private void stopLeScan(BluetoothLeScanner leScanner) {
+        mScanning = false;
+        leScanner.stopScan(leScanCallback);
+    }
+
+    private double calculateDistance(int RSSI, int txPower) {
+
+        double ratio = RSSI*1.0/txPower;
+        if (ratio < 1.0) {
+            return Math.pow(ratio,10);
+        }
+        else {
+            double accuracy =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;
+            return accuracy;
+        }
+    }
+    private void determineTxPower(ScanResult result)
+    {
+        if(result.getTxPower() == ScanResult.TX_POWER_NOT_PRESENT)
+        {
+            txPower = -69;
+        }
+        else {txPower = result.getTxPower();}
+    }
+
+    private void calibrateAt1m(ScanResult result) {
+        determineTxPower(result);
+        double distance = calculateDistance(result.getRssi(), txPower);
+        int RSSI = result.getRssi();
+        String address = result.getDevice().getAddress();
+        int i = 0;
+
+        if(distance > 0.8 && distance < 1.2) {
+            txPower = (txPower + RSSI)/2;
+        }
+        MACandRSSIMap.replace(address, txPower);
     }
 }
 
